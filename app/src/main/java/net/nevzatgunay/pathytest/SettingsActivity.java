bc08167ce2945bd62064392d2bade096e8a9f733
Nevzat Günay <gunaynevzat@yandex.com>
2017-03-25 20:46:39 +0300

    main activity

---
+++ app/src/main/java/net/nevzatgunay/pathytest/SettingsActivity.java
@@ -0,0 +1,28 @@
+package net.nevzatgunay.pathytest;
+
+/**
+ * Created by Nevzat on 3/25/2017.
+ */
+
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.os.Bundle;
+
+public class SettingsActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+    }
+
+    public static class MyPreferenceFragment extends PreferenceFragment{
+
+        @Override
+        public void onCreate(final Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
+}
---
+++ app/src/main/java/net/nevzatgunay/pathytest/TwoFragment.java
@@ -0,0 +1,34 @@
+package net.nevzatgunay.pathytest;
+
+/**
+ * Created by Nevzat on 3/22/2017.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import net.nevzatgunay.pathytest.R;
+
+
+public class TwoFragment extends Fragment{
+
+    public TwoFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_two, container, false);
+    }
+
+}
